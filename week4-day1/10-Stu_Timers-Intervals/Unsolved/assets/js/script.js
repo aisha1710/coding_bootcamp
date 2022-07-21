8ca4a1666f947f69b77b6aac2d4c92fe9bac3622
aisha <faisha879@gmail.com>
2022-07-21 22:11:08 +0100

    commiting classwork

--- week4-day1/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
+++ week4-day1/10-Stu_Timers-Intervals/Unsolved/assets/js/script.js
@@ -1,21 +1,31 @@
-var timerEl = document.getElementById('countdown');
-var mainEl = document.getElementById('main');
+var timerEl = document.getElementById("countdown");
+var mainEl = document.getElementById("main");
 
 var message =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var words = message.split(' ');
+  "Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.";
+var words = message.split(" ");
 
 function countdown() {
   var timeLeft = 5;
-
   // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+
   var timeInterval = setInterval(function () {
+    timeLeft--;
+
+    timerEl.textContent = timeLeft + " seconds remaining";
+
+    if (timeLeft === 0) {
+      clearInterval(timeInterval);
+      displayMessage();
+    }
+
     //
     // YOUR CODE HERE
     //
-  });
+  }, 1000);
 }
 
+countdown();
 // Displays the message one word at a time
 function displayMessage() {
   var wordCount = 0;
--- week4-day2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ week4-day2/20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -2,6 +2,14 @@ var container = document.querySelector(".container");
 
 container.addEventListener("click", function (event) {
   var element = event.target;
+  var state = element.getAttribute("data-state");
+  if (state === "hidden") {
+    element.setAttribute("data-state", "visible");
+    element.textContent = element.getAttribute("data-number");
+  } else {
+    element.setAttribute("data-state", "hidden");
+    element.textContent = "";
+  }
 
   // TODO: Complete function
 });
